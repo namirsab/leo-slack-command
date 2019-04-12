e7c513a7e9e615c1c51a6918867d39cd51dbbdb6
Namir Sayed-Ahmad Baraza <namirsab@gmail.com>
2019-04-13 00:52:57 +0200

    Make it work with different languages

--- server.js
+++ server.js
@@ -4,29 +4,74 @@ const fastify = require('fastify')({ logger: true })
 const fetch = require('node-fetch');
 const cheerio = require('cheerio');
 
+const preferences = {
+    defaultLanguageSetting: 'spanisch-deutsch',
+}
+
+const userPrefs = {};
+
+const parseArguments = ({ argsText, defaultLanguageSetting }) => {
+    const args = argsText.split(' ');
+    console.log 
+    let languageSetting = defaultLanguageSetting;
+    let sourceLanguageCode = 'es';
+    let term; 
+    if (args.length > 1) {
+        languageSetting = args[0];
+        term = args[1];
+    } else {
+        term = args[0];
+    }
+
+    if (languageSetting.includes('englisch')) {
+        sourceLanguageCode = 'en';
+    }
+
+    return {
+        languageSetting,
+        sourceLanguageCode,
+        term,
+    };
+};
+
 
 
 fastify.register(require('fastify-formbody'))
 
+
+
 // Declare a route
 fastify.post('/leo', async (request, reply) => {
-    console.log('hello');
-    const { text } = request.body;
-    const url = `https://dict.leo.org/spanisch-deutsch/${text}`;
-    console.log(request)
+    const { text, user_id } = request.body;
+    console.log({ user_id });
+    const defaultLanguageSetting = userPrefs[user_id] || preferences.defaultLanguageSetting;
+    const { languageSetting, term, sourceLanguageCode } = parseArguments({ 
+        argsText: text, 
+        defaultLanguageSetting,
+    });
+    console.log({
+        text,
+        languageSetting,
+        term,
+        sourceLanguageCode
+    });
+
+    userPrefs[user_id] = languageSetting;
+
+    const url = `https://dict.leo.org/${languageSetting}/${term}`;
     const html = await fetch(url).then(r => r.text());
     const $ = cheerio.load(html);
 
     const results = $('[data-dz-ui=dictentry]');
     const response = [...results.map((index, dictEntry) => {
         const $dictEntry = $(dictEntry);
-        const spanishText = $dictEntry.find('td[lang="es"]').text();
+        const spanishText = $dictEntry.find(`td[lang="${sourceLanguageCode}"]`).text();
         const germanText = $dictEntry.find('td[lang="de"]').text();
         return `${spanishText} <-> ${germanText}`;
     }).get()].join('\n');
 
     return {
-        text: `*Results for "${text}"*`,
+        text: `*Results for "${term}"*`,
         attachments: [{
             title: 'Leo',
             text: response
